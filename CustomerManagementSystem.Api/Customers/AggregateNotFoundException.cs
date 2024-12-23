0b38adf55d0ccd4df6c5486f09cd8a5d71f413ff
Shahab <shahabganji@gmail.com>
2024-12-23 18:57:50 +0100

    chore: remove unused constructor from AggregateNotFoundException`
    The constructor using an email parameter was removed as it is no longer used. This cleanup ensures the codebase remains streamlined and avoids unnecessary methods.

--- CustomerManagementSystem.Api/Customers/AggregateNotFoundException.cs
+++ CustomerManagementSystem.Api/Customers/AggregateNotFoundException.cs
@@ -2,10 +2,6 @@ namespace CustomerManagementSystem.Api.Customers;
 
 public class AggregateNotFoundException : Exception
 {
-    public AggregateNotFoundException(string email) : base($"Aggregate with email: {email} was not found")
-    {
-    }
-
     public AggregateNotFoundException(Guid customerId) : base($"Aggregate with id: {customerId} was not found")
     {
     }
