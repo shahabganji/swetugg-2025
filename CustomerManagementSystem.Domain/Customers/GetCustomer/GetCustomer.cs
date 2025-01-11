9f0eadd95ffd4215f7873b4cb82ca8b01186ab90
Shahab <shahabganji@gmail.com>
2025-01-11 11:55:38 +0100

    feat: add validation for unconfirmed customer in GetCustomer
    Ensure that an exception is thrown if a customer's registration is not confirmed. This improves data integrity by preventing unconfirmed customers from being processed.

--- CustomerManagementSystem.Domain/Customers/GetCustomer/GetCustomer.cs
+++ CustomerManagementSystem.Domain/Customers/GetCustomer/GetCustomer.cs
@@ -4,13 +4,21 @@ namespace CustomerManagementSystem.Domain.Customers.GetCustomer;
 
 public sealed record GetCustomer(Guid Id);
 
-
 public sealed class GetCustomerHandler(IEventStore store)
 {
     public async Task<Maybe<Customer>> Handle(GetCustomer query)
     {
         var stream = new EventStream<Customer>(store, query.Id);
-        var customer = await stream.GetEntity();
-        return customer;
+
+        var customerMaybe = await stream.GetEntity();
+
+        if (customerMaybe.IsNone)
+            return customerMaybe;
+        
+        var customer = customerMaybe.UnwrappedValue;
+        if (customer.IsRegistrationConfirmed == false)
+            throw new InvalidOperationException("Customer is not confirmed");
+
+        return customerMaybe;
     }
 }
